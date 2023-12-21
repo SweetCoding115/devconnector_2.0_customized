b4dcc56ae78f661ab73e7cabdb4fb7c14d4bfc1a
@ttractivenes$ <moriahbuckridge96115@gmail.com>
2023-12-21 22:10:29 +0800

    I prefer useselector hooks

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,79 +1,19 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Dashboard = ({
-  getCurrentProfile,
-  deleteAccount,
-  auth: { user },
-  profile: { profile }
-}) => {
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const profile = useSelector((state) => state.profile.profile);
   useEffect(() => {
-    getCurrentProfile();import React, { useEffect } from 'react';
-    import { Link } from 'react-router-dom';
-    import PropTypes from 'prop-types';
-    import DashboardActions from './DashboardActions';
-    import Experience from './Experience';
-    import Education from './Education';
-    import { getCurrentProfile, deleteAccount } from '../../actions/profile';
-    import { useDispatch, useSelector } from 'react-redux';
-    
-    const Dashboard = () => {
-      const dispatch = useDispatch();
-      const user = useSelector((state) => state.auth.user);
-      const profile = useSelector((state) => state.profile.profile);
-      useEffect(() => {
-        dispatch(getCurrentProfile());
-      }, [dispatch]);
-    
-      return (
-        <section className="container">
-          <h1 className="large text-primary">Dashboard</h1>
-          <p className="lead">
-            <i className="fas fa-user" /> Welcome {user && user.name}
-          </p>
-          {profile !== null ? (
-            <>
-              <DashboardActions />
-              <Experience experience={profile.experience} />
-              <Education education={profile.education} />
-    
-              <div className="my-2">
-                <button
-                  className="btn btn-danger"
-                  onClick={() => dispatch(deleteAccount())}
-                >
-                  <i className="fas fa-user-minus" /> Delete My Account
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <p>You have not yet setup a profile, please add some info</p>
-              <Link to="/create-profile" className="btn btn-primary my-1">
-                Create Profile
-              </Link>
-            </>
-          )}
-        </section>
-      );
-    };
-    
-    Dashboard.propTypes = {
-      getCurrentProfile: PropTypes.func.isRequired,
-      deleteAccount: PropTypes.func.isRequired,
-      auth: PropTypes.object.isRequired,
-      profile: PropTypes.object.isRequired
-    };
-    
-    export default Dashboard;
-    
-  }, [getCurrentProfile]);
+    dispatch(getCurrentProfile());
+  }, [dispatch]);
 
   return (
     <section className="container">
@@ -88,7 +28,10 @@ const Dashboard = ({
           <Education education={profile.education} />
 
           <div className="my-2">
-            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+            <button
+              className="btn btn-danger"
+              onClick={() => dispatch(deleteAccount())}
+            >
               <i className="fas fa-user-minus" /> Delete My Account
             </button>
           </div>
@@ -112,11 +55,4 @@ Dashboard.propTypes = {
   profile: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  profile: state.profile
-});
-
-export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
-  Dashboard
-);
+export default Dashboard;
