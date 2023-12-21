295807f5a78c57b37e661f844cfb70bc43e44a51
@ttractivenes$ <moriahbuckridge96115@gmail.com>
2023-12-21 17:31:33 +0800

    I used useselector hook on client part beside using mapstatetoprops. it's kinda tough.

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Spinner from '../layout/Spinner';
 
-const PrivateRoute = ({
-  component: Component,
-  auth: { isAuthenticated, loading }
-}) => {
+const PrivateRoute = ({ component: Component }) => {
+  const [isAuthenticated, loading] = useSelector((state) => [
+    state.auth.isAuthenticated,
+    state.auth.loading
+  ]);
   if (loading) return <Spinner />;
   if (isAuthenticated) return <Component />;
 
@@ -18,8 +19,4 @@ PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
-  auth: state.auth
-});
-
-export default connect(mapStateToProps)(PrivateRoute);
+export default PrivateRoute;
